f6de51f87c34e645d079f4f34c30c59d7299719f
Michele Bucelli <michele.bucelli@polimi.it>
2024-11-25 08:54:23 +0000

    Upload labs 03 and 04

---
+++ lab-03/src/lab-03.cpp
@@ -0,0 +1,16 @@
+#include <deal.II/base/convergence_table.h>
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "Poisson1D.hpp"
+
+// Main function.
+int
+main(int /*argc*/, char * /*argv*/[])
+{
+  // Todo.
+
+  return 0;
+}
