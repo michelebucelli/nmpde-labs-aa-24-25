2fb6523a6f0f3c83162fe77e347f9b7dc58bbb96
Michele Bucelli <michele.bucelli@polimi.it>
2024-12-15 16:14:13 +0000

    Upload lab 08

---
+++ lab-08/src/lab-08-exercise1.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+
+#include "Heat.hpp"
+
+// Main function.
+int
+main(int argc, char *argv[])
+{
+  Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
+  const unsigned int               mpi_rank =
+    Utilities::MPI::this_mpi_process(MPI_COMM_WORLD);
+
+  // TODO.
+
+  return 0;
+}
